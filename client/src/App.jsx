874a845fc51fcf31eeb4e3ced41f4390165d2374
nika robakidze <nika70252@gmail.com>
2026-03-14 15:54:08 +0400

    front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
 
+  const [data, setData] = useState([])
+
+
+  useEffect(() => {
+    fetch("https://job-tracker-t0qo.onrender.com/")
+    .then(jsonResponse => jsonResponse.json())
+    .then(incomingData => setData(incomingData))
+
+
+  }, [])
   return (
     <>
-      <h1>Hello World</h1>
+      {
+        data.map((item) => <h1 key={item.id}>{item.company}</h1>)
+      }
     </>
   )
 }
