83a7cb2259b706a612d0a366c6632054801668de
nika robakidze <nika70252@gmail.com>
2026-05-08 15:38:38 +0400

    error-handling

--- server/router/mainRouter.js
+++ server/router/mainRouter.js
@@ -1,33 +1,56 @@
 import express from "express"
 import sql from "../db/db.js"
+import { errorMiddleware } from "../middleware/errorMiddleware.js"
 
 export const appRouter = express.Router()
 
-appRouter.get('/jobs', async (req, res) => {
+appRouter.get('/', async (req, res) => {
     const jobs = await sql`SELECT * FROM jobs;`
     res.json(jobs)
 })
 
-appRouter.post("/jobs", async (req, res) => {
+appRouter.post("/", async (req, res) => {
     const [ sendData ] = await sql `INSERT INTO jobs(company, position, status) VALUES (${req.body.company}, ${req.body.position}, ${req.body.status}) RETURNING *;`
     res.json(sendData)
 })
 
-appRouter.get("/jobs/:id", async (req, res) => {
-    const { id } = req.params
-    const searchById = await sql `SELECT * FROM jobs WHERE id = ${id};`
-    res.json(searchById)
-})
-
-appRouter.delete("/jobs/:id", async (req, res) => {
+appRouter.delete("/:id", async (req, res) => {
     const { id } = req.params
     const removeData = await sql `DELETE FROM jobs WHERE id = ${id};`
     res.json(removeData)
 })
 
-appRouter.patch("/jobs/:id", async (req, res) => {
-    const { id } = req.params
-    const [ patchData ] = await sql `UPDATE jobs SET company = ${req.body.company}, position = ${req.body.position}, status = ${req.body.status} WHERE id = ${id};`
-    res.json(patchData)
+appRouter.patch("/:id", async (req, res) => {
+    try{
+        const { id } = req.params
+        const [ patchData ] = await sql `UPDATE jobs SET company = ${req.body.company}, position = ${req.body.position}, status = ${req.body.status} WHERE id = ${id};`
+
+        if (!searchById) {
+            return res.status(500).json({ message: "Cant change data" })
+        }
+
+        res.json(patchData)
+    } catch(error){
+        next(error)
+    }
 })
-    
+
+appRouter.get("/:id", async (req, res, next) => {
+    try{
+        const { id } = req.params
+
+        if(isNaN(id)){
+            return res.status(400).json({ message: "Wrong type of id" })
+        }
+
+        const [ searchById ] = await sql `SELECT * FROM jobs WHERE id = ${id};`
+
+        if (!searchById) {
+            return res.status(404).json({ message: "Job not found" })
+        }
+
+        res.json(searchById)
+    } catch(error) {
+        next(error)
+    }
+})
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { appRouter } from "./router/mainRouter.js"
+import { errorMiddleware } from "./middleware/errorMiddleware.js"
 
 const PORT = process.env.PORT || 8000
 
@@ -9,6 +10,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use('/', appRouter)
+app.use('/jobs', appRouter)
+
+app.use(errorMiddleware)
 
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
